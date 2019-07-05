ec38995b6b5103ae8f07f06372d79def76546cff
Michael <madscamper@hotmail.com>
2019-07-05 23:22:59 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export type hans = string | number;
+export { PathType } from './object/models/path-type';
+export { TraversablePathType } from './object/models/traversable-path.type';
+export { ObjectPath } from './object/object-path.class';
+export { PathWizard } from './object/path-wizard';
