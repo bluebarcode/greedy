3c9837c479e6d71ac341a8cd9f067feb522927be
Michael <madscamper@hotmail.com>
2019-10-24 17:46:16 +0200

    Exports adjusted - version adjusted

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { ObjectPath } from './implementation/object-path.class';
 export { PathWizard } from './implementation/path-wizard';
+export { $filter } from './operators/filter';
+export { $map } from './operators/map';
+export { $store } from './operators/store';
+export { $unite } from './operators/unite';
 export { PathType } from './typings/path-type';
 export { TraversableGreedyType } from './typings/traversable-greedy.type';
